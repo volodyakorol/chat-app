3f8ae37c5fb76a01a7be89efdfc0c8e5dd631a59
volodyakorol <volodymyr.korol@coaxsoft.com>
2022-11-13 15:11:14 +0200

    feat: add app layout

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,13 +1,14 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const isProd = process.env.NODE_ENV === 'production';
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
 
   beforeSend(event) {
-    if (event.exception) {
+    if (event.exception && isProd) {
       Sentry.showReportDialog({ eventId: event.event_id });
     }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
 import { AppProps } from 'next/app';
 
 import { WithProviders } from '@/shared/provider';
+import { AppLayout } from '@/widgets/layouts';
 
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@/shared/styles/index.scss';
+import 'antd/dist/antd.css';
+
+config.autoAddCss = false;
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <WithProviders>
-      <div className='app'>
+      <AppLayout>
         <Component {...pageProps} />
-      </div>
+      </AppLayout>
     </WithProviders>
   );
 }
