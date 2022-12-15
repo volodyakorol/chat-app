e4fb0d35b278990446dc8c259a4d61d1750ca181
volodyakorol <volodymyr.korol@coaxsoft.com>
2022-12-15 09:25:07 +0200

    fix: fix update modules

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,14 +5,13 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import { notification } from 'antd';
 import type { AppProps } from 'next/app';
 
+import useSocket from '@/shared/hook/useSocket';
+import { GetMessagesResponse } from '@/shared/types';
 import { AppLayout } from '@/widgets/layouts';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import 'antd/dist/antd.css';
 import 'react-advanced-cropper/dist/style.css';
 import '@/shared/styles/index.scss';
-import useSocket from '@/shared/hook/useSocket';
-import { GetMessagesResponse } from '@/shared/types';
 
 config.autoAddCss = false;
 
